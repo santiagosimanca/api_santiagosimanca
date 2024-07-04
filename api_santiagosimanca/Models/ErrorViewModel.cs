866afe16d3e126f5d326dfafa7113e60c9773c15
santiagosimanca <154481716+santiagosimanca@users.noreply.github.com>
2024-07-04 02:09:16 -0500

    Agregar archivos de proyecto.

---
+++ api_santiagosimanca/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace api_santiagosimanca.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ api_santiagosimanca/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function searchProduct() {
+    var term = document.getElementById("searchTerm").value;
+    fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${term}`)
+        .then(response => response.json())
+        .then(data => {
+            var results = document.getElementById("results");
+            results.innerHTML = "";
+            data.results.forEach(product => {
+                results.innerHTML += `
+          <div>
+            <h3>${product.title}</h3>
+            <img src="${product.thumbnail}" alt="${product.title}">
+            <p>Precio: ${product.price}</p>
+          </div>
+        `;
+            });
+        });
+}
+
